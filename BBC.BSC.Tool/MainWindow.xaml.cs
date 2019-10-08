9975569d57a249cd9a46b5ee86d1d4b7bf8d7cce
Kristan Webb <kristan.webb@bbc.co.uk>
2019-10-08 13:06:47 +0100

    add requirement for double click handler

--- BBC.BSC.Tool/MainWindow.xaml.cs
+++ BBC.BSC.Tool/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Input;
 using System.Windows.Media;
 //using System.Threading;
 
