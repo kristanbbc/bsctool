3dee89707d7cef96bb2d81678d3077ec7d2bf6cb
Kristan Webb <kristan.webb@bbc.co.uk>
2021-05-24 18:55:51 +0100

    Remove Info Display tab and code

--- BBC.BSC.Tool/Properties/AssemblyInfo.cs
+++ BBC.BSC.Tool/Properties/AssemblyInfo.cs
@@ -49,6 +49,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.4.57")]
-[assembly: AssemblyFileVersion("1.6.4.57")]
-[assembly: AssemblyInformationalVersion("1.6.19")]
+[assembly: AssemblyVersion("1.6.4.59")]
+[assembly: AssemblyFileVersion("1.6.4.59")]
+[assembly: AssemblyInformationalVersion("1.6.21")]
