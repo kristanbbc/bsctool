d37ca9a5d0e436ddda703bfe5a074552664aaf26
Kristan Webb <kristan.webb@bbc.co.uk>
2021-06-29 18:57:23 +0100

    Add first attempt at connecting to vCenter

--- BBC.BSC.Tool/MyResult.cs
+++ BBC.BSC.Tool/MyResult.cs
@@ -7,6 +7,8 @@
         public string Ip { get; set; }
         public string Description { get; set; }
         public string OperatingSystem { get; set; }
+        public string Tag { get; set; }
+
         public MyResult()
         {
 
---
+++ BBC.BSC.Tool/vCenter/VmList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BBC.BSC.Tool.vCenter
+{
+    public class VmList
+    {
+
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class Value
+        {
+            [JsonProperty("memory_size_MiB")]
+            public int Memory_size_MiB { get; set; }
+            [JsonProperty("vm")]
+            public string Vm { get; set; }
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("power_state")]
+            public string Power_state { get; set; }
+            [JsonProperty("cpu_count")]
+            public int Cpu_count { get; set; }
+        }
+
+        public class Root
+        {
+            [JsonProperty("value")]
+            public List<Value> Value { get; set; }
+        }
+    }
+}
