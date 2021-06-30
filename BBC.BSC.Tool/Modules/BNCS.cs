7880a292bd7daae8c55d5388a306d28e11534473
Kristan Webb <kristan.webb@bbc.co.uk>
2021-06-30 12:05:28 +0100

    tidy usings

--- BBC.BSC.Tool/Modules/BNCS.cs
+++ BBC.BSC.Tool/Modules/BNCS.cs
@@ -1,9 +1,4 @@
 ﻿using MaterialDesignThemes.Wpf;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BBC.BSC.Tool.Modules
 {
