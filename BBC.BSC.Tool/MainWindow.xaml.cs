a5017033e4ce490fb7815e4c1b882c1636aebdff
Kristan Webb <kristan.webb@bbc.co.uk>
2021-07-02 17:24:20 +0100

    Refactor logging to separate class

--- BBC.BSC.Tool/MainWindow.xaml.cs
+++ BBC.BSC.Tool/MainWindow.xaml.cs
@@ -23,9 +23,6 @@ using MaterialDesignThemes.Wpf;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using NLog;
-using NLog.Config;
-using NLog.Targets;
-using NLog.Targets.ElasticSearch;
 using Timer = System.Timers.Timer;
 
 namespace BBC.BSC.Tool
@@ -59,39 +56,9 @@ namespace BBC.BSC.Tool
             Version version = Assembly.GetExecutingAssembly().GetName().Version;
 
             Title = "BSC Tool - Version " + version;
-            var config = new LoggingConfiguration();
-            var consoleTarget = new ColoredConsoleTarget
-            {
-                // ReSharper disable once StringLiteralTypo
-                Layout = "${time} ${pad:padding=3:inner=${threadid}} ${message} ${exception:format=tostring}"
-            };
-            var elasticSearchTarget = new ElasticSearchTarget
-            {
-                Index = "bsctool1",
-                IncludeAllProperties = true
-            };
-            elasticSearchTarget.Fields.Add(new Field { Name = "user", Layout = "${windows-identity:userName=True:domain=False}" });
-            elasticSearchTarget.Fields.Add(new Field { Name = "host", Layout = "${machinename}" });
-            elasticSearchTarget.Fields.Add(new Field { Name = "thread", Layout = "${threadid}" });
-            elasticSearchTarget.Fields.Add(new Field { Name = "threadname", Layout = "${threadname}" });
-            elasticSearchTarget.Fields.Add(new Field { Name = "version", Layout = "${assembly-version}" });
-#if DEBUG
-            elasticSearchTarget.Fields.Add(new Field { Name = "build", Layout = "DEBUG" });
 
-#else
-            elasticSearchTarget.Fields.Add(new NLog.Targets.ElasticSearch.Field() { Name = "build", Layout = "RELEASE" });
-
-#endif
-
-            elasticSearchTarget.Layout = "${message} ${exception:format=tostring}";
-
-            elasticSearchTarget.Uri = @"http://3gbbmdbels1000:9200";
-            config.AddRule(LogLevel.Info, LogLevel.Fatal, elasticSearchTarget);
-
-            config.AddRule(LogLevel.Trace, LogLevel.Fatal, consoleTarget);
-
-            logger = LogManager.GetCurrentClassLogger();
-            LogManager.Configuration = config;
+            var logging = new Logging();
+            logger = logging.initLogger();
 
             logger.Info("BSC Tool {0} starting.", FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion);
             var watcher = new Timer
