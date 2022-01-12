59fbbd5362490057c5ec2b41f4cfb7000abc0d71
Kristan Webb <kristan.webb@bbc.co.uk>
2022-01-12 15:17:50 +0000

    addng unit tests

---
+++ BBC.BSC.Tool.UnitTest/Modules/BncsTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BBC.BSC.Tool.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaterialDesignThemes.Wpf;
+
+namespace BBC.BSC.Tool.Modules.Tests
+{
+    [TestClass()]
+    public class BncsTests
+    {
+        [TestMethod()]
+        public void GetPackIconKindTest()
+        {
+            
+            Assert.AreEqual(Bncs.GetPackIconKind("vnc"), PackIconKind.Computer);
+
+        }
+    }
+}
